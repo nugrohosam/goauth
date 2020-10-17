54743222a114cae2a0c2033cc95ae6997e4455fb
nugroho samiyono <nugrohosamiyono@gmail.com>
2020-10-17 12:53:49 +0700

    Asadfsaf

--- tests/utilities/db-cleaner.go
+++ tests/utilities/db-cleaner.go
@@ -28,20 +28,22 @@ func DbCleaner(t *testing.T) {
 	cleanDB, err := db.Query(getAllTable)
 	if err != nil {
 		t.Error(err)
+		return
 	}
 	defer cleanDB.Close()
 
 	var tableName string
-	t.Log("Clean DB")
+	db.Exec("SET FOREIGN_KEY_CHECKS=0;")
 	for cleanDB.Next() {
 		cleanDB.Scan(&tableName)
 		if strings.Contains(tableName, "schema_migrations") {
 			continue
 		}
 
-		t.Log("Query : " + tableName)
 		if _, err := db.Exec(tableName); err != nil {
 			t.Error(err)
+			return
 		}
 	}
+	db.Exec("SET FOREIGN_KEY_CHECKS=1;")
 }
