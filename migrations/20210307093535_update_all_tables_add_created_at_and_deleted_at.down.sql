f000997525214987f68188bae420809e242083a0
nugroho samiyono <nugrohosamiyono@gmail.com>
2021-03-07 10:47:49 +0700

    Fix | Migration

---
+++ migrations/20210307093535_update_all_tables_add_created_at_and_deleted_at.down.sql
@@ -0,0 +1,8 @@
+ALTER TABLE users DROP COLUMN created_at;
+ALTER TABLE users DROP COLUMN updated_at;
+ALTER TABLE roles DROP COLUMN created_at;
+ALTER TABLE roles DROP COLUMN updated_at;
+ALTER TABLE permissions DROP COLUMN created_at;
+ALTER TABLE permissions DROP COLUMN updated_at;
+ALTER TABLE role_permission DROP COLUMN created_at;
+ALTER TABLE role_permission DROP COLUMN updated_at;
---
+++ migrations/20210307093535_update_all_tables_add_created_at_and_deleted_at.up.sql
@@ -0,0 +1,8 @@
+ALTER TABLE users ADD created_at DATETIME NULL;
+ALTER TABLE users ADD updated_at DATETIME NULL;
+ALTER TABLE roles ADD created_at DATETIME NULL;
+ALTER TABLE roles ADD updated_at DATETIME NULL;
+ALTER TABLE permissions ADD created_at DATETIME NULL;
+ALTER TABLE permissions ADD updated_at DATETIME NULL;
+ALTER TABLE role_permission ADD created_at DATETIME NULL;
+ALTER TABLE role_permission ADD updated_at DATETIME NULL;
